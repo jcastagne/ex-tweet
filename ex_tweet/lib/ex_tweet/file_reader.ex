1a0a67fcb3eb0f7ec8abe28f32b298f183bb3dec
julien castagné <juliencastagne@MacBook-Pro-de-julien.local>
2019-08-13 20:17:30 -0500

    Updating fileReader to read a random line from tweets file

--- ex_tweet/lib/ex_tweet/file_reader.ex
+++ ex_tweet/lib/ex_tweet/file_reader.ex
@@ -5,5 +5,6 @@ defmodule ExTweet.FileReader do
         |> String.split("\n")
         |> Enum.map(&String.trim/1)
         |> Enum.filter(&String.length(&1) <= 140)
+        |> Enum.random()
     end
 end
